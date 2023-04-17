f00c47f2437b0d902fd5df062a7852f25e65bc84
IsraEB <sr.enriquez@yahoo.com>
2023-04-17 05:22:39 -0600

    Tratando de arreglar las mamadas inarreglables de Anmiel

--- micromouse.py
+++ micromouse.py
@@ -1,5 +1,6 @@
 import serial
 import time
+from pprint import pprint
 
 def flood_fill(maze, start, goal_area):
     maze[start[0]][start[1]] = 0
@@ -41,7 +42,7 @@ def send_phase_change_to_robot(ser):
     time.sleep(0.5)
 
 def init_maze(rows, cols):
-    maze = [[1000000] * cols for _ in range(rows)]
+    maze = [[None] * cols for _ in range(rows)]
     return maze
 
 def ir_values_to_walls(sensor_values, ir_threshold):
@@ -63,7 +64,7 @@ def update_maze(maze, x, y, walls):
         maze[x+1][y] = -1
 
 def process_sensor_data(sensor_data, current_position):
-    sensor_values = sensor_data.split(',')
+    sensor_values = sensor_data
     sensor_dict = {
         "IRL": int(sensor_values[1]),
         "IRD": int(sensor_values[3]),
@@ -76,8 +77,7 @@ def process_sensor_data(sensor_data, current_position):
 def read_and_update_maze(data, maze, current_position, IR_THRESHOLD, goal_area):
     sensor_dict, current_position = process_sensor_data(data, current_position)
     walls = ir_values_to_walls(sensor_dict, IR_THRESHOLD)
-    update_maze(maze, current_position[0], current_position[1], walls)
-    return maze, current_position, goal_area
+    return maze, walls, current_position, goal_area
 
 
 def print_maze(maze):
@@ -124,6 +124,130 @@ def get_start_position(maze, corner):
     elif corner == 'bottom_right':
         return (len(maze) - 1, len(maze[0]) - 1)
 
+def rotate_matrix(matrix):
+    transposed = []
+    for row in zip(*matrix):
+        transposed.append(list(row))
+    rotated = []
+    for row in transposed:
+        rotated.append(row[::-1])
+    return rotated
+
+def determineNextMovement(walls, maze, current_position,currentN, heading):
+    int_walls = [1 if walls["left"] else 0, 1 if walls["front"] else 0, 1 if walls["right"] else 0]
+
+    pprint(maze)
+
+    newCurrentN = currentN+1
+
+    nextMovement = None
+
+    if(int_walls == [0,0,0]):
+        nextMovement =  ["right", "right"]
+    if(int_walls == [0,0,1]):
+        maze = rotate_matrix(maze)
+        if(heading == "north"):
+            maze[current_position[0]+1][current_position[1]] = newCurrentN
+        elif(heading == "south"):
+            maze[current_position[0]-1][current_position[1]] = newCurrentN
+        elif(heading == "east"):
+            maze[current_position[0]][current_position[1]+1] = newCurrentN
+        elif(heading == "west"):
+            maze[current_position[0]][current_position[1]-1] = newCurrentN
+
+        nextMovement =  ["right","front"]
+    if(int_walls == [0,1,0]):
+        if(heading == "north"):
+            maze[current_position[0]+1][current_position[1]] = newCurrentN
+        elif(heading == "south"):
+            maze[current_position[0]-1][current_position[1]] = newCurrentN
+        elif(heading == "east"):
+            maze[current_position[0]][current_position[1]+1] = newCurrentN
+        elif(heading == "west"):
+            maze[current_position[0]][current_position[1]-1] = newCurrentN
+        nextMovement =  ["front"]
+    if(int_walls == [0,1,1]): # Bifurcación
+        nextMovement =  [["front"],["right","front"]]
+    if(int_walls == [1,0,0]):
+        nextMovement =  ["left","front"]
+    if(int_walls == [1,0,1]): # Bifurcación
+        nextMovement =  ["front"]
+    if(int_walls == [1,1,0]): # Bifurcación
+        nextMovement =  ["front"]
+    if(int_walls == [1,1,1]): # Bifurcación
+        nextMovement =  ["front"]
+
+    return nextMovement, maze
+
+
+def step(ser,phase,maze,goal_area,start,current_position,heading,IR_THRESHOLD,visited, currentN):
+    line = None
+
+    # IRL,0,IRD,0,IRF,600,SR1,0,SR2,0
+
+    line = input("Entra: ")
+
+    if(ser != None):
+        line = ser.readline().decode().strip()
+
+    print(line)
+
+    data = line.split(",")
+    # Leer datos del sensor y actualizar la matriz del laberinto
+    maze, walls, current_position, goal_area = read_and_update_maze(data, maze, current_position, IR_THRESHOLD, goal_area)
+
+    print(current_position)
+
+    visited.add(current_position)
+    # Verificar si el robot ha llegado a la meta
+    if not current_position in goal_area:
+        if not all_nodes_visited(maze, visited) and phase == 1:
+
+            nextMovement, maze = determineNextMovement(walls, maze, current_position, currentN, heading)
+
+            print(nextMovement)
+
+            if(all_nodes_visited(maze, visited)):
+                phase = 2
+
+        if phase == 2:
+            # En la fase 2, envía los movimientos calculados al robot
+            send_movements_to_robot(movements, ser)
+            return False,ser,phase,maze,goal_area,start,current_position,heading,IR_THRESHOLD,visited, currentN
+    # Mostrar la matriz del laberinto en tiempo real
+    print_maze(maze)
+
+    return True,ser,phase,maze,goal_area,start,current_position,heading,IR_THRESHOLD,visited, currentN
+
+
+def markInMaze(maze,start,currentN):
+    maze[start[0]][start[1]]=currentN
+    return currentN + 1
+
+def mainWithoutConnection():
+    ser = None
+    phase = 1
+    maze = init_maze(12, 7)
+
+    currentN = 0 # Número que se marcará en el laberinto
+
+    goal_area = get_goal_area(maze)
+    start = get_start_position(maze, 'bottom_left')  # Reemplazar
+
+    currentN = markInMaze(maze,start,currentN)
+
+    current_position = start
+    heading = 'north'
+    IR_THRESHOLD = 500  # Ajustar según la detección de obstáculos de los sensores IR
+    visited = set()
+    visited.add(start) #// Checkcheckcheck
+
+
+    while True:
+        willContinue,ser,phase,maze,goal_area,start,current_position,heading,IR_THRESHOLD,visited, currentN = step(ser,phase,maze,goal_area,start,current_position,heading,IR_THRESHOLD,visited, currentN)
+        if(not willContinue):
+            break
+
 
 def main():
     ser = serial.Serial('COM3', 9600)
@@ -141,29 +265,11 @@ def main():
 
     while True:
         if ser.in_waiting > 0:
-            line = ser.readline().decode().strip()
-            data = line.split(",")
-            # Leer datos del sensor y actualizar la matriz del laberinto
-            maze, current_position, goal_area = read_and_update_maze(data, maze, current_position, IR_THRESHOLD, goal_area)
-            visited.add(current_position)
-            # Verificar si el robot ha llegado a la meta
-            if current_position in goal_area:
-                if all_nodes_visited(maze, visited) and phase == 1:
-                    time.sleep(15)
-                    # Cambia a la fase 2 (seguimiento de la ruta óptima)
-                    phase = 2
-                    start = current_position
-                    # Calcula la ruta óptima y envía un comando al robot para indicar el inicio de la fase 2
-                    movements = flood_fill(maze, start, goal_area)
-                    print("Movimientos calculados:", movements)
-                    send_phase_change_to_robot(ser)
+            willContinue = step(ser,phase,maze,goal_area,start,current_position,heading,IR_THRESHOLD,visited)
+            if(not willContinue):
+                break
 
-                else:
-                    # En la fase 2, envía los movimientos calculados al robot
-                    send_movements_to_robot(movements, ser)
-                    break
-            # Mostrar la matriz del laberinto en tiempo real
-            print_maze(maze)
 
 if __name__ == '__main__':
-    main()
+    mainWithoutConnection()
+
