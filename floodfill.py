7c365171011b2edb91a5a98c30e9fac1f40fd4ac
IsraEB <sr.enriquez@yahoo.com>
2023-04-18 01:26:57 -0600

    Integración con Arduino

--- floodfill.py
+++ floodfill.py
@@ -1,11 +1,62 @@
+import serial
+
+from pprint import pprint
+
+arduino = None
+
+try:
+    arduino = serial.Serial('COM10', 9600)
+except:
+    raise Exception("No se ha podido conectar al puerto")
+
+# while True:
+#     if arduino.in_waiting > 0:
+#         line = arduino.readline().decode().strip()
+
+def print_maze():
+    # Print filled maze
+    for row in maze:
+        print(row)
+    print()
+
 def floodfill(maze, start, end):
     queue = [start]
     visited = set()
     distance = {start: 0}
     parent = {}  # Map of child: parent
 
+    last_visited = None
+    
     while queue:
         current = queue.pop(0)
+        
+        print("Current: ", current)
+        
+        print(last_visited, current)
+        
+        if(last_visited != current and last_visited != None):
+            
+            arduinoMovesTo = (
+                last_visited[0]-current[0],
+                current[1]-last_visited[1]
+            )
+            
+            print("Para llegar a esta posición, el arduino se tiene que mover a (",arduinoMovesTo[0],",",arduinoMovesTo[1], ")")
+            
+            arduino.write((str(arduinoMovesTo[0])+"\n").encode())
+            arduino.write((str(arduinoMovesTo[1])+"\n").encode())
+            while True:
+                if arduino.in_waiting > 0:
+                    line = arduino.readline().decode().strip()
+                    print(line)
+                    break
+                
+        last_visited = current
+           
+        # Genera error
+        # if(current in visited):
+        #     continue 
+        
         visited.add(current)
 
         # Check neighbors
@@ -22,13 +73,19 @@ def floodfill(maze, start, end):
             visited.add(neighbor)
             distance[neighbor] = distance[current] + 1
             parent[neighbor] = current
+            
+            maze[neighbor[0]][neighbor[1]] = distance[neighbor]
+            
             if neighbor == end:
                 # Build path
                 path = [end]
                 while path[-1] != start:
                     path.append(parent[path[-1]])
                 path.reverse()
+                print_maze()
                 return maze, path
+            
+        print_maze()
 
     # Fill maze with distance values
     for i in range(len(maze)):
